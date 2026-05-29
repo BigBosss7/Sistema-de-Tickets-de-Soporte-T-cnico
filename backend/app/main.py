6d7b11796a197452748a715d2bbb843e1a28ecf8
BigBosss7 <saul.b07@gmail.com>
2026-05-29 14:33:51 -0600

    Add FastAPI setup and healthendpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,14 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI(
     title="Support Ticket System",
+    description="API para gestionar tickets de soporte técnico.",
     version="0.1.0"
 )
 
-@app.get("/")
+@app.get(
+    "/",
+    summary="Health check inicial",
+    description="Verifica que la API está funcionando correctamente."
+)
 def home():
     return {
+        "status": "ok",
         "message": "Support Ticket System API is running"
     }
 
+@app.get("/health",
+    summary="Health check",
+    description="Verifica que la API está funcionando correctamnete"
+)
+def health():
+    return {
+        "api": "running",
+        "version": "0.1.0"
+    }
 
-    
